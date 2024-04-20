951d62dc6504dd88b17202a0163e9177ea6fa7aa
Ayşenur Yavuz <aysenur.fcb@gmail.com>
2024-04-20 23:38:09 +0300

    project created

---
+++ app/src/test/java/com/getir/patika/getir_android_kotlin_bootcamp_finalcase_aysenur_/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.getir.patika.getir_android_kotlin_bootcamp_finalcase_aysenur_
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
