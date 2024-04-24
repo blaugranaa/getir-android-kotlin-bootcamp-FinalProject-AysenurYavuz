e81d515b1fb9d5b89b5ecd3d6e6800b9d0dc9e83
Ayşenur Yavuz <aysenur.fcb@gmail.com>
2024-04-24 19:29:14 +0300

    updated

--- app/src/main/java/com/getir/patika/getir_android_kotlin_bootcamp_finalcase_aysenur_/MainActivity.kt
+++ app/src/main/java/com/getir/patika/getir_android_kotlin_bootcamp_finalcase_aysenur_/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.getir.patika.getir_android_kotlin_bootcamp_finalcase_aysenur_
 
+
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var cardSum:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        cardSum = findViewById(R.id.cardSum)
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        val fragment = HomeFragment()
+        transaction.replace(R.id.frameLayout, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+
+        cardSum.setOnClickListener {
+
+
+
+
+
         }
+
     }
+
+
+
+
+
+
+
 }
